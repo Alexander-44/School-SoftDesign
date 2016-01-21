a67990406481dcd688635ed1e04ef2fb6d9a5e7b
Alexander-44 <lomik-44@yandex.ru>
2016-01-21 17:41:15 +0400

    --попытка сделать первый pull request-- добавлено: 1)струтура проекта 2)неотрефакторенный логгер 3)методы жизненного цикла Activity

--- app/src/main/java/com/softdesign/school/MainActivity.java
+++ app/src/main/java/com/softdesign/school/MainActivity.java
@@ -3,11 +3,52 @@ package com.softdesign.school;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.softdesign.school.utils.*;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Lg log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log.i(getLocalClassName(),"Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log.i(getLocalClassName(), "Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log.i(getLocalClassName(), "Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log.i(getLocalClassName(), "Stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        log.i(getLocalClassName(), "Restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        log.i(getLocalClassName(),"Destroy");
+    }
+
 }
